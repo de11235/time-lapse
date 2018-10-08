c37ce2bdc995ebc8a843fa1546bfcbcc77e2ca93
de11235 <deikens@hotmail.com>
2018-10-07 21:38:12 -0500

    First Commit

---
+++ snapshot.py
@@ -0,0 +1,9 @@
+from picamera import PiCamera
+from time import sleep
+
+camera = PiCamera()
+camera.resolution = (1024, 768)
+camera.start_preview()
+sleep(2)
+camera.capture('image.jpg')
+#camera.stop_preview()
